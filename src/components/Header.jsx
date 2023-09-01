055f3b8426a184fca983c76c69cfd31614260143
Diego Tenjo <tenjodiego961@gmail.com>
2023-09-01 10:42:14 -0500

    Change name of project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,21 @@ export const Header = () => {
     return (
         <nav className="flex w-full h-20 bg-red-400 justify-between items-center">
             <div className="ml-8">
-                <Link to="/">
-                    <span>Fisica</span>
+                <Link to="/" className='block text-center'>
+                    <h1 className='text-xl font-extrabold text-white'>DimenSwell</h1>
+                    <span className='text-sm font-light text-gray-200'>Explora la expansión térmica de los sólidos.</span>
                 </Link>
             </div>
             <div className="mr-12">
                 <ul className="flex w-44 justify-between">
                     <li>
-                        <Link to="/r1">R1</Link>
+                        <Link to="/r1" className='p-2 bg-white rounded-md hover:font-semibold'>R1</Link>
                     </li>
                     <li>
-                        <Link to="/r2">R2</Link>
+                        <Link to="/r2" className='p-2 bg-white rounded-md hover:font-semibold'>R2</Link>
                     </li>
                     <li>
-                        <Link to="/r3">R3</Link>
+                        <Link to="/r3" className='p-2 bg-white rounded-md hover:font-semibold'>R3</Link>
                     </li>
                 </ul>
             </div>
